ebfce0305b774731c13f79b339977d1499460562
yangxiao <yangxiao02@bilibili.com>
2023-02-28 14:22:14 +0800

    first add bili

--- agent/index.ts
+++ agent/index.ts
@@ -1,22 +1,12 @@
-import { log } from "./logger.js";
+interface IPresenter {
+  isShowCujian: Java.Method;
+}
+Java.perform(function () {
+  const presenter = <IPresenter>(
+    Java.use("com.bcut.homepage.presenter.CreatePresenter")
+  );
 
-const header = Memory.alloc(16);
-header
-    .writeU32(0xdeadbeef).add(4)
-    .writeU32(0xd00ff00d).add(4)
-    .writeU64(uint64("0x1122334455667788"));
-log(hexdump(header.readByteArray(16) as ArrayBuffer, { ansi: true }));
-
-Process.getModuleByName("libSystem.B.dylib")
-    .enumerateExports()
-    .slice(0, 16)
-    .forEach((exp, index) => {
-        log(`export ${index}: ${exp.name}`);
-    });
-
-Interceptor.attach(Module.getExportByName(null, "open"), {
-    onEnter(args) {
-        const path = args[0].readUtf8String();
-        log(`open() path="${path}"`);
-    }
+  presenter.isShowCujian.implementation = () => {
+    return true;
+  };
 });
